42ce624325ccf4e2ba525aad976061d4d604dfdb
gmrowe <garrett.m.rowe@gmail.com>
2024-03-05 19:13:14 -0700

    Extra packages autoadded to custom.el

--- emacs/.config/emacs/custom.el
+++ emacs/.config/emacs/custom.el
@@ -6,7 +6,7 @@
  '(custom-safe-themes
    '("1d51919e9a075a4f56396681dc99adecefa152ab29819178403cbf6ae5bc0dd8" default))
  '(package-selected-packages
-   '(flycheck-clj-kondo zprint-mode smartparens smart-parens cider rust-mode ivy flycheck-rust rustic projectile flycheck company org-superstar move-text which-key smex simplicity-theme)))
+   '(paredit clojure-mode flycheck-clj-kondo zprint-mode smartparens smart-parens cider rust-mode ivy flycheck-rust rustic projectile flycheck company org-superstar move-text which-key smex simplicity-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
